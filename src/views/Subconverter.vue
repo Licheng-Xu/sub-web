18e1b6ad03c1ce5fcde71b70915e27ff95f2a8c0
Licheng-Xu <licheng_xu1997@outlook.com>
2021-04-05 14:54:11 +0800

    feat: 添加自己的后端

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,10 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "http://127.0.0.1:25500/sub?" }],
+        backendOptions: [
+          { value: "http://127.0.0.1:25500/sub?" },
+          { value: "https://sub.lcxu.site/sub?" }
+        ],
         remoteConfig: [
           {
             label: "universal",
